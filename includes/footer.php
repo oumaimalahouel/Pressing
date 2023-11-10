1c6c81c358ec3d6248807c7472c978577987b19e
Oumaima Lahouel <lahoueloumaima1@gmail.com>
2023-11-10 10:56:08 +0100

    Footer and header

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+﻿
+     
+	 <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    
+     <script src="assets/js/bootstrap.min.js"></script>
+	 
+     </div>
+    </div>
+ </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,35 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta charset="utf-8" />
+  <title>PosCaisse</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <!-- Debut Appel Fichiers CSS -->
+  
+  <link href="assets/css/bootstrap.css" rel="stylesheet">
+  
+  <link href="assets/css/normalize.css" rel="stylesheet" type="text/css">
+  
+  <link href="assets/css/webflow.css" rel="stylesheet" type="text/css">
+  
+  <link href="assets/css/personalisation.css" rel="stylesheet" type="text/css">
+  
+  <link href="assets/css/print.css" rel="stylesheet" type="text/css">
+  
+  <link href="assets/css/caisse.webflow.css" rel="stylesheet" type="text/css">
+  
+  <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+  
+  <link href="assets/css/font.css" rel="stylesheet">
+  
+  <!-- Fin Appel Fichiers CSS -->
+  
+  <!-- Debut Appel Fichiers JS -->
+  
+  
+  <script src="assets/js/jquery-1.8.2.js" type="text/javascript"></script>
+  
+ 
+  
+  <!-- Fin Appel Fichiers JS -->
